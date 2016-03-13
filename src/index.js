a25c2c2c311593479a0441fdb2f71b18abc4eefa
Pablo Chiappetti <pchiappetti@gmail.com>
2016-03-13 20:24:26 -0300

    add callback

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-import providers from './providers'
+import * as providers from './providers'
 
-var search = function(type, options) {
-    switch (type) {
-        case 'movie':
-            return searchMovie(options)
-        case 'episode':
-            return searchEpisode(options)
-        default:
-            return searchMovie(options)
-    }
+var search = function(type, options, cb) {
+  switch (type) {
+    case 'movie':
+      return searchMovie(options, cb)
+    case 'episode':
+      return searchEpisode(options, cb)
+    default:
+      return searchMovie(options, cb)
+  }
 }
 
-const searchMovie = function(options) {
+const searchMovie = function(options, cb) {
   return 'searching a Movie'
 }
 
-const searchEpisode = function(options) {
-  var provider = providers.findByName('tusubtitulo')
-  console.log(provider)
-  return 'searching an Episode'
+const searchEpisode = function(options, cb) {
+  const { provider } = options
+  var Provider = providers.findByName(provider)
+  Provider.find(options, cb)
 }
 
-search('episode')
+// search('episode', {
+//   provider: 'tusubtitulo',
+//   tvShow: 'new girl',
+//   season: 5,
+//   episode: 6,
+//   language: 'es_es'
+// }, function(subtitles) {
+//   subtitles[0].download('newgirl.srt')
+// })
